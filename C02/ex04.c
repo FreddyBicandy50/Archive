61f70dfcce0b1a03f01b03b336e039ab6764c69c
FreddyBicandy50 <fredos112033@gmail.com>
2024-04-23 13:07:48 +0300

    upload

---
+++ C02/ex04.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <unistd.h>
+
+char *ft_strstr(char *str, char *to_find)
+{
+    int i = 0;
+    int j = 0;
+    char *position = NULL;
+
+    while (str[i] != '\0' && to_find[j] != '\0')
+    {
+        if (str[i] == to_find[j])
+        {
+            if (j == 0)
+                position = &str[i];
+            j++;
+        }
+        else
+        {
+            position = NULL;
+            j = 0;
+        }
+        i++;
+    }
+    return position;
+}
+
+// void main(void)
+// {
+//     char str[] = "FreddyHello World";
+//     char *find = "fddy";
+//     char *res = ft_strstr(str, find);
+//     if (res == NULL)
+//         printf("\nsubstring not found\n");
+//     else
+//         printf("\nsubstring found\n");
+// }
