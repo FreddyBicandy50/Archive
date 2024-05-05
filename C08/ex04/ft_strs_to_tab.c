ff7e97985e96679c62e0b2207f48cd1ba1253d2d
FreddyBicandy50 <fredos112033@gmail.com>
2024-05-05 09:49:51 +0300

    gitupload

--- C08/ex04/ft_strs_to_tab.c
+++ C08/ex04/ft_strs_to_tab.c
@@ -1,26 +1,70 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_strs_to_tab.c                                   :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: fbicandy <fbicandy@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/05/04 18:06:07 by fbicandy          #+#    #+#             */
-/*   Updated: 2024/05/04 22:47:39 by fbicandy         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "ft_stock_str.h"
-#include <stdio.h>
+#include "stdio.h"
+#include "stdlib.h"
 
-struct s_stock_str *ft_strs_to_tab(int ac, char **av)
+int ft_strlen(char *str)
 {
-	t_stock_str *arr = (t_stock_str *) malloc((ac + 1) * sizeof(t_stock_str));
-	
+	int len;
+
+	len = 0;
+	while (str[len] != '\0')
+		len++;
+	return len;
+}
+
+char *ft_strdup(char *src)
+{
+	int i;
+
+	i = 0;
+	char *dest = malloc(sizeof(char *) * ft_strlen(src + 1));
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return dest;
+}
+
+struct s_stock_str **ft_strs_to_tab(int ac, char **av)
+{
+	int i;
+	t_stock_str **tab = malloc(sizeof(t_stock_str *) * ac + 1);
+
+	if (tab == NULL)
+		return (NULL);
+	i = 0;
+	while (i < ac)
+	{
+		tab[i] = malloc(sizeof(t_stock_str *));
+		tab[i]->size = ft_strlen(av[i] + 1);
+		tab[i]->str = av[i];
+		tab[i]->copy = ft_strdup(tab[i]->str);
+		i++;
+	}
+	tab[ac] = NULL;
+
+	return (tab);
 }
 
 int main(void)
 {
-
-	return (0);
+	char *message[] = {"Hello", "World", "We", "are", "Christians"};
+	int size = 5;
+	t_stock_str **result = ft_strs_to_tab(size, message);
+	if (result == NULL)
+	{
+		printf("Memorie allocation Failed!");
+		return (1);
+	}
+	int i = 0;
+	while (result[i] != NULL)
+	{
+		printf("str=%s | ", result[i]->str);
+		printf("size=%d | ", result[i]->size);
+		printf("copy=%s\n", result[i]->copy);
+		i++;
+	}
+	return 0;
 }
