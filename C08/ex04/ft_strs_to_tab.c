db51ae916b1f93a5e1612239fb78a411b87f0388
FreddyBicandy50 <fredos112033@gmail.com>
2024-05-09 12:21:24 +0300

    gitupload

--- C08/ex04/ft_strs_to_tab.c
+++ C08/ex04/ft_strs_to_tab.c
@@ -1,102 +1,55 @@
 #include "ft_stock_str.h"
+#include "ft_show_tab.c"
 #include "stdlib.h"
-#include "unistd.h"
 
 int ft_strlen(char *str)
 {
 	int len;
+
 	len = 0;
 	while (str[len] != '\0')
 		len++;
 	return len;
 }
 
-char *ft_strdup(char *str)
+char *ft_strcpy(char *src)
 {
-	char *dest;
 	int i;
+	char *dest;
 
-	dest = malloc(sizeof(char *) * ft_strlen(str) + 1);
+	dest = malloc(sizeof(char *) * (ft_strlen(src) + 1));
 	if (dest == NULL)
-		return NULL;
+		return (0);
 	i = 0;
-	while (str[i] != '\0')
+	while (src[i] != '\0')
 	{
-		dest[i] = str[i];
+		dest[i] = src[i];
 		i++;
 	}
 	dest[i] = '\0';
 	return dest;
 }
 
-struct s_stock_str **ft_strs_to_tab(int ac, char **av)
+struct s_stock_str *ft_strs_to_tab(int ac, char **av)
 {
-	t_stock_str **tab = malloc(sizeof(t_stock_str *) * ac + 1);
 	int i;
+	t_stock_str *tab;
 
+	tab = (t_stock_str *) malloc(sizeof(t_stock_str) * (ac + 1));
+	if (tab == NULL)
+		return (NULL);
 	i = 0;
 	while (i < ac)
 	{
-		tab[i] = malloc(sizeof(t_stock_str *));
-		tab[i]->str = av[i];
-		tab[i]->size = ft_strlen(av[i]);
-		tab[i]->copy = ft_strdup(tab[i]->str);
+		tab[i].str = av[i];
+		tab[i].size = ft_strlen(av[i]);
+		tab[i].copy = ft_strcpy(av[i]);
 		i++;
 	}
-	tab[i] = NULL;
+	tab[i].size = 0;
+	tab[i].str = 0;
+	tab[i].copy = 0;
 	return tab;
-}
-
-void ft_putchar(char digit)
-{
-	write(1, &digit, 1);
-	write(1, "\n", 1);
-}
-
-void ft_putstr(char *str)
-{
-	while (*str != '\0')
-	{
-		write(1, str, 1);
-		str++;
-	}
-	write(1, "\n", 1);
-	return;
-}
-
-void ft_putnbr(int nb)
-{
-	if (nb == -2147483648)
-	{
-		write(1, "-2147483648", 11);
-		return;
-	}
-	else if (nb < 0)
-	{
-		ft_putchar('-');
-		nb *= -1;
-	}
-	if (nb >= 10)
-	{
-		ft_putnbr(nb / 10);
-		ft_putnbr(nb % 10);
-	}
-	else
-		ft_putchar(nb + '0');
-}
-
-void ft_show_tab(struct s_stock_str **par)
-{
-	int i;
-
-	i = 0;
-	while (par[i] != NULL)
-	{
-		ft_putstr(par[i]->str);
-		ft_putnbr(par[i]->size);
-		ft_putstr(par[i]->copy);
-		i++;
-	}
 }
 
 int main(void)
@@ -106,5 +59,4 @@ int main(void)
 
 	size = 6;
 	ft_show_tab(ft_strs_to_tab(size, message));
-	return (0);
 }
