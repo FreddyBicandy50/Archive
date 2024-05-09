ed618435960e4d4acb774d8b89a9b4f373192052
FreddyBicandy50 <fredos112033@gmail.com>
2024-05-09 12:21:38 +0300

    gitupload

--- C08/ex05/ft_show_tab.c
+++ C08/ex05/ft_show_tab.c
@@ -1,36 +1,36 @@
+
 #include "ft_stock_str.h"
+#include "unistd.h"
+#include "stdio.h"
 #include "stdlib.h"
 
-void ft_putchar(char digit)
+void	ft_putstr(char *str)
 {
-	write(1, &digit, 1);
-	write(1, "\n", 1);
+	int	i;
+
+	i = 0;
+	while (str[i] !='\0'){
+		write(1, &str[i], 1);
+		i++;
+	}
 }
 
-void ft_putstr(char *str)
+void	ft_putchar(char c)
 {
-	while (*str != '\0')
-	{
-		write(1, str, 1);
-		str++;
-	}
-	write(1, "\n", 1);
-	return;
+	write(1, &c, 1);
 }
 
-void ft_putnbr(int nb)
+void	ft_putnbr(int nb)
 {
-	if (nb == -2147483648)
-	{
-		write(1, "-2147483648", 11);
-		return;
-	}
-	else if (nb < 0)
+	long	i;
+
+	i = nb;
+	if (nb < 0)
 	{
 		ft_putchar('-');
-		nb *= -1;
+		i *= -1;
 	}
-	if (nb >= 10)
+	if (nb > 9)
 	{
 		ft_putnbr(nb / 10);
 		ft_putnbr(nb % 10);
@@ -39,16 +39,19 @@ void ft_putnbr(int nb)
 		ft_putchar(nb + '0');
 }
 
-void ft_show_tab(struct s_stock_str **par)
+void	ft_show_tab(struct s_stock_str *par)
 {
-	int i;
+	int	i;
 
 	i = 0;
-	while (par[i] != NULL)
+	while (par[i].size != 0)
 	{
-		ft_putstr(par[i]->str);
-		ft_putnbr(par[i]->size);
-		ft_putstr(par[i]->copy);
+		ft_putstr(par[i].str);
+		ft_putchar('\n');
+		ft_putnbr(par[i].size);
+		ft_putchar('\n');
+		ft_putstr(par[i].copy);
+		ft_putchar('\n');
 		i++;
 	}
 }
