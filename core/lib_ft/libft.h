61acb3469fdecedd18b85ca6bc6d387a5cce08a4
Frederic Bicandy <fbicandy@c1r5s6.42beirut.com>
2024-06-12 18:47:47 +0300

    Upload

---
+++ core/lib_ft/libft.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fbicandy <fbicandy@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/10 14:37:11 by fbicandy          #+#    #+#             */
+/*   Updated: 2024/06/12 17:34:51 by fbicandy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+#define LIBFT_H
+#include "ft_isdigit.c"
+#include "ft_isalpha.c"
+#include "ft_strchr.c"
+#include "ft_tolower.c"
+#include "ft_strlen.c"
+
+#endif
