931d67346040605870c88e690a1eb7c1b09ea2ec
Frederic Bicandy <fbicandy@c1r2s1.42beirut.com>
2024-06-13 23:20:22 +0300

    Upload

--- core/lib_ft/check.sh
+++ core/lib_ft/check.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Run check 
-clear  && cd /home/fbicandy/42Beirut/core/libft-war-machine && bash grademe.sh > res 
+clear  && cd /home/fbicandy/42Beirut/libft-war-machine && bash grademe.sh > res 
 
 # FT
 ft="ft_"
