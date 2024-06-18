7d6b44c7ca748832f905f6594674d5cf88ad8046
FreddyBicandy50 <fbicandy@FREDDY-PC>
2024-06-18 20:32:40 +0300

    Upload

--- core/lib_ft/ft_itoa.c
+++ core/lib_ft/ft_itoa.c
@@ -6,20 +6,22 @@
 /*   By: fbicandy <fbicandy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/17 23:24:19 by fbicandy          #+#    #+#             */
-/*   Updated: 2024/06/18 12:27:56 by fbicandy         ###   ########.fr       */
+/*   Updated: 2024/06/18 20:30:04 by fbicandy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 
-static int	ft_numlen(int n)
+int	ft_len(int num)
 {
 	int	len;
+	int	n;
 
 	len = 0;
+	n = num;
 	if (n <= 0)
 		len = 1;
-	while (n != 0)
+	while (n != '\0')
 	{
 		n /= 10;
 		len++;
@@ -29,26 +31,28 @@ static int	ft_numlen(int n)
 
 char	*ft_itoa(int n)
 {
-	char			*str;
+	char			*num;
 	int				len;
-	unsigned int	num;
+	unsigned int	n_abs;
 
-	len = ft_numlen(n);
-	str = (char *)malloc(len + 1);
-	if (!str)
+	len = ft_len(n);
+	num = (char *)malloc((len + 1) * (sizeof(char *)));
+	if (!num)
 		return (NULL);
-	str[len] = '\0';
+	num[len] = '\0';
+	if (n == 0)
+		num[0] = '0';
 	if (n < 0)
 	{
-		str[0] = '-';
-		num = (unsigned int)(-n);
+		num[0] = '-';
+		n_abs = (unsigned int)(-n);
 	}
 	else
-		num = (unsigned int)n;
-	while (len > 0 && str[len - 1] != '-')
+		n_abs = (unsigned int)n;
+	while (n_abs > 0)
 	{
-		str[--len] = (num % 10) + '0';
-		num /= 10;
+		num[--len] = (n_abs % 10) + '0';
+		n_abs /= 10;
 	}
-	return (str);
+	return (num);
 }
---
+++ core/lib_ft/tempCodeRunnerFile.sh
@@ -0,0 +1,2 @@
+
+grep "$strmapi" re
