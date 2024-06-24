4cea2a13f9c68cfce97034b87a3ed1b7f9787e42
Frederic Bicandy <fbicandy@c1r2s1.42beirut.com>
2024-06-24 22:09:31 +0300

    Upload

--- core/ft_printf/ft_printf.c
+++ core/ft_printf/ft_printf.c
@@ -6,14 +6,56 @@
 /*   By: fbicandy <fbicandy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/24 17:49:53 by fbicandy          #+#    #+#             */
-/*   Updated: 2024/06/24 19:07:19 by fbicandy         ###   ########.fr       */
+/*   Updated: 2024/06/24 22:09:05 by fbicandy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "stdio.h"
+#include <stdio.h>
+#include <stdarg.h>
+#include <unistd.h>
+#include "libft/libft.h"
 
-int	ft_printf(const char *input, ...)
+int ft_printf(const char *input, ...)
 {
-	printf("%c", *input);
-	return (0);
+	va_list args;
+	int count;
+
+	va_start(args, input);
+	count = 0;
+	while (*input != '\0')
+	{
+		if (*input == '%')
+		{
+			input++;
+			if (*input == 'i')
+				ft_putnbr_fd(va_arg(args, int), 1);
+			if (*input == 'd')
+				ft_putnbr_fd(va_arg(args, int), 1);
+			if (*input == 'c')
+				ft_putchar_fd((char)va_arg(args, int), 1);
+			if (*input == 's')
+				ft_putstr_fd(va_arg(args, char *), 1);
+			count++;
+		}
+		else
+		{
+			ft_putchar_fd(*input, 1);
+			count++;
+		}
+		input++;
+	}
+	va_end(args);
+	return (count);
 }
+
+// int main(void)
+// {
+// 	char first_name = 'f';
+// 	char *lastname = "bicandy";
+// 	char *option1 = "FBI";
+// 	char *option2 = "CANDY";
+// 	int foutry_two = 42;
+// 	int age = 23;
+// 	ft_printf("Hello %i my name is=%c%s and i have=%d (yrs old)\n\t\t\"IT NOT %s,OR %s\"\n", foutry_two, first_name, lastname,age, option1, option2);
+// 	return (0);
+// }
--- core/ft_printf/includes/ft_printf.h
+++ core/ft_printf/includes/ft_printf.h
@@ -6,12 +6,13 @@
 /*   By: fbicandy <fbicandy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/24 18:02:14 by fbicandy          #+#    #+#             */
-/*   Updated: 2024/06/24 18:03:41 by fbicandy         ###   ########.fr       */
+/*   Updated: 2024/06/24 19:11:47 by fbicandy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
+int	ft_printf(const char *input, ...);
 
 #endif
