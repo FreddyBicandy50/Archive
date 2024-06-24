2d11d46b2ee5c4d4bde4320ac4dd9b3ee9b70688
Frederic Bicandy <fbicandy@c1r2s1.42beirut.com>
2024-06-24 22:15:36 +0300

    Upload

--- core/ft_printf/ft_printf.c
+++ core/ft_printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: fbicandy <fbicandy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/24 17:49:53 by fbicandy          #+#    #+#             */
-/*   Updated: 2024/06/24 22:09:05 by fbicandy         ###   ########.fr       */
+/*   Updated: 2024/06/24 22:14:39 by fbicandy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,13 @@
 #include <unistd.h>
 #include "libft/libft.h"
 
-int ft_printf(const char *input, ...)
+int	ft_printf(const char *input, ...)
 {
-	va_list args;
-	int count;
+	va_list	args;
 
-	va_start(args, input);
-	count = 0;
 	while (*input != '\0')
 	{
+		va_start(args, input);
 		if (*input == '%')
 		{
 			input++;
@@ -35,17 +33,13 @@ int ft_printf(const char *input, ...)
 				ft_putchar_fd((char)va_arg(args, int), 1);
 			if (*input == 's')
 				ft_putstr_fd(va_arg(args, char *), 1);
-			count++;
 		}
 		else
-		{
 			ft_putchar_fd(*input, 1);
-			count++;
-		}
 		input++;
 	}
 	va_end(args);
-	return (count);
+	return (0);
 }
 
 // int main(void)
@@ -56,6 +50,8 @@ int ft_printf(const char *input, ...)
 // 	char *option2 = "CANDY";
 // 	int foutry_two = 42;
 // 	int age = 23;
-// 	ft_printf("Hello %i my name is=%c%s and i have=%d (yrs old)\n\t\t\"IT NOT %s,OR %s\"\n", foutry_two, first_name, lastname,age, option1, option2);
+// 	ft_printf("Hello %i my name is=%c%s and i
+// have=%d (yrs old)\n\t\t\"IT NOT %s,OR %s\"\n",
+// foutry_two, first_name, lastname,age, option1, option2);
 // 	return (0);
 // }
