ebcab4cb28c1821c384ec74744dd24a7b06ae9eb
Frederic Bicandy <fbicandy@c2r6s1.42beirut.com>
2024-07-03 16:17:32 +0300

    Upload

--- born2beroot/monitoring.sh
+++ born2beroot/monitoring.sh
@@ -1,13 +1,26 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    monitoring.sh                                      :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: fbicandy <fbicandy@student.42.fr>          +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2024/07/02 20:32:41 by fbicandy          #+#    #+#              #
+#    Updated: 2024/07/03 16:02:59 by fbicandy         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
 clear
 
 arch=$(uname -a)
 physical_cpu=$(cat /proc/cpuinfo | grep "physical id"  | sort | uniq | wc -l)
-get_ram_usage=$(free -h | grep "Mem:" | awk '{fprint($2)}')
-get_ram_total=$()
 
-sudo wall "
-                System Monitoring Info
-        #Architecture: $arch
-        #pCPU: $physical_cpu | vCPU: $physical_cpu
-        #Memory Usage:$get_ram_usage
-    "
+Tram=$(free -h | grep "Mem:"  | awk '{print($2)}')
+Uram=$(free -h | grep "Mem:" | awk '{print($3)}')
+Pram=$(free | grep "Mem:"  | awk '{printf("%.2f"),$3/$2*100}')"%"
+
+echo $arch
+echo "          Ram"
+echo "Total:"$Tram
+echo "Usage:"$Uram
+echo "Memory Usage:" $Tram/${Uram}MB "($Pram)"
