91066ba808434052cc83a13e4e6a7d35b12c78fb
FreddyBicandy50 <fbicandy@FREDDY-PC>
2024-08-01 20:01:52 +0300

    Upload

--- core/exam/42-School-Exam-Rank-02/Level 3/*ft_atoi_base/ft_atoi_base.c
+++ core/exam/42-School-Exam-Rank-02/Level 3/*ft_atoi_base/ft_atoi_base.c
@@ -1,92 +1,52 @@
-// char	to_lower(char c)
-// {
-// 	if (c >= 'A' && c <= 'Z')
-// 		return (c + ('a' - 'A'));
-// 	return (c);
-// }
-
-// int get_digit(char c, int digits_in_base)
-// {
-// 	int max_digit;
-// 	if (digits_in_base <= 10)
-// 		max_digit = digits_in_base + '0';
-// 	else
-// 		max_digit = digits_in_base - 10 + 'a';
-// 	if (c >= '0' && c <= '9' && c <= max_digit)
-// 		return (c - '0');
-// 	else if (c >= 'a' && c <= 'f' && c <= max_digit)
-// 		return (10 + c - 'a');
-// 	else
-// 		return (-1);
-// }
-
-// int ft_atoi_base(const char *str, int str_base)
-// {
-// 	int result = 0;
-// 	int sign = 1;
-// 	int digit;
-
-// 	if (*str == '-')
-// 	{
-// 		sign = -1;
-// 		++str;
-// 	}
-
-// 	while ((digit = get_digit(to_lower(*str), str_base)) >= 0)
-// 	{
-// 		result = result * str_base;
-// 		result = result + (digit * sign);
-// 		++str;
-// 	}
-// 	return (result);
-// }
-
 #include <stdio.h>
-char ft_tolower(char c)
+
+char to_lower(char c)
 {
 	if (c >= 'A' && c <= 'Z')
-		return c - 32;
-	return c;
+		return (c + ('a' - 'A'));
+	return (c);
 }
 
-int get_digit(char c, int base)
+int get_digit(char c, int digits_in_base)
 {
 	int max_digit;
-	if (base <= 10)
-		max_digit = base - '0';
+	if (digits_in_base <= 10)
+		max_digit = digits_in_base + '0';
 	else
-		max_digit = base - 10 + 'a';
+		max_digit = digits_in_base - 10 + 'a';
 	if (c >= '0' && c <= '9' && c <= max_digit)
 		return (c - '0');
 	else if (c >= 'a' && c <= 'f' && c <= max_digit)
 		return (10 + c - 'a');
-	return (-1);
+	else
+		return (-1);
 }
 
-int atoi_base(char *str, int base)
+int ft_atoi_base(const char *str, int str_base)
 {
-	int res;
+	int result = 0;
 	int sign = 1;
 	int digit;
+
 	if (*str == '-')
 	{
 		sign = -1;
-		str++;
+		++str;
 	}
-	res = 0;
-	while ((digit = get_digit(ft_tolower(*str), base)) >= 10)
+
+	while ((digit = get_digit(to_lower(*str), str_base)) >= 0)
 	{
-		res = res * base;
-		res = res + (digit * base);
-		str++;
+		result = result * str_base;
+		result = result + (digit * sign);
+		++str;
 	}
-	return (res);
+	return (result);
 }
 
 int main(void)
 {
-	char *message = "Freddy @1120";
-	int base = 10;
-	printf("%d", atoi_base(message, base));
+	char *message = "50";
+	int base = 8;
+	printf("%d\n", ft_atoi_base(message, base));
 	return (0);
 }
