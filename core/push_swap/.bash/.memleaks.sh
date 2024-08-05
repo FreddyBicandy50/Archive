527ca2a2b8971ec6e16a0ab1f155c3382ee626eb
FreddyBicandy50 <fbicandy@FREDDY-PC>
2024-08-05 11:29:29 +0300

    Upload

--- core/push_swap/.bash/.memleaks.sh
+++ core/push_swap/.bash/.memleaks.sh
@@ -3,8 +3,8 @@ pushd "/home/fbicandy/42Beirut/core/push_swap/" || exit
 
 
 # Compile
-make && make clean
+make fclean  && make && make clean
 clear
 valgrind --leak-check=full  ./push_swap
 
-rm a.out
+rm -rf push_swap
