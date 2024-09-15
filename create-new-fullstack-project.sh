fa7c4554dab1a52459c92f6a891fc026763ea808
FreddyBicandy50 <fbicandy@Freddy-PC>
2024-09-15 18:19:29 +0300

    Upload

--- create-new-fullstack-project.sh
+++
@@ -1,9 +0,0 @@
-echo "project name"
-read name
-mkdir ~/draft/$name && pushd ~/draft/$name
-temp = $name + "app"
-sudo npx create-expo-app $temp --template blank
-temp = $name + "backend"
-sudo npx create-next-app@latest $temp
-
-
