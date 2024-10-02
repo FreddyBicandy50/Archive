42332b3466f547284310c13798bee6dc6170d39e
FreddyBicandy50 <fbicandy@Freddy-PC>
2024-10-02 21:53:14 +0300

    upload

--- Flutter/my_firstapp/lib/main.dart
+++ Flutter/my_firstapp/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:my_firstapp/widgets/card_item.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
             colorScheme: ColorScheme.fromSeed(seedColor: Colors.white10),
             useMaterial3: true),
-        home: const MyHomePage(title: "home page"));
+        home: const MyHomePage(title: "Home"));
   }
 }
 
@@ -29,47 +30,6 @@ class MyHomePage extends StatefulWidget {
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
-
-// class _MyHomePageState extends State<MyHomePage> {
-//   int _counter = 0;
-
-//   void _incrementCounter() {
-//     setState(() {
-//       _counter++;
-//     });
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-//         title: Text(widget.title),
-//       ),
-//       body: Center(
-//         child: Column(
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           crossAxisAlignment: CrossAxisAlignment.end,
-//           children: <Widget>[
-//             const Text(
-//               'test You have pushed the button this many times:',
-//             ),
-//             Text(
-//               '$_counter',
-//               style: Theme.of(context).textTheme.headlineMedium,
-//             ),
-//             const Text("push me")
-//           ],
-//         ),
-//       ),
-//       floatingActionButton: FloatingActionButton(
-//         onPressed: _incrementCounter,
-//         tooltip: 'Increment',
-//         child: const Icon(Icons.access_alarm),
-//       ), // This trailing comma makes auto-formatting nicer for build methods.
-//     );
-//   }
-// }
 
 class _MyHomePageState extends State<MyHomePage> {
   int _bananas = 0;
@@ -87,25 +47,31 @@ class _MyHomePageState extends State<MyHomePage> {
           backgroundColor: Theme.of(context).colorScheme.inversePrimary,
           title: Text(widget.title),
         ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+        body: const SafeArea(
+          child: Row(
             children: [
-              Text(
-                "Eat The banana 🍌",
-                style: Theme.of(context).textTheme.headlineLarge,
+              CardItem(
+                imageUrl: 'https://via.placeholder.com/100',
+                itemName: 'Item 1',
+                itemPrice: '\$29.99',
               ),
-              Text(
-                '$_bananas',
-                style: Theme.of(context).textTheme.titleMedium,
-              )
+              CardItem(
+                imageUrl: 'https://via.placeholder.com/100',
+                itemName: 'Item 1',
+                itemPrice: '\$29.99',
+              ),
+              CardItem(
+                imageUrl: 'https://via.placeholder.com/100',
+                itemName: 'Item 1',
+                itemPrice: '\$29.99',
+              ),
             ],
           ),
         ),
         floatingActionButton: FloatingActionButton(
           onPressed: _increase,
           tooltip: "get banana",
-          child: const Icon(Icons.breakfast_dining_sharp),
+          child: const Icon(Icons.add_a_photo),
         ));
   }
 }
