c482d04d772a34039898859d150bd49213a09294
FreddyBicandy50 <fbicandy@Freddy-PC>
2024-10-04 15:52:44 +0300

    upload

--- Flutter/code_challenge/android/app/src/main/kotlin/com/example/code_challenge/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.code_challenge
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- Flutter/my_firstapp/android/app/src/main/kotlin/com/example/my_firstapp/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.my_firstapp
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
