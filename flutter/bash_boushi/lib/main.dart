6ea217df388cc0cca3c860b2b22620a994dd1c5e
FreddyBicandy50 <fbicandy@Freddy-PC>
2024-10-05 14:03:57 +0300

    upload

---
+++ flutter/bash_boushi/lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      backgroundColor: const Color.fromARGB(255, 174, 174, 174),
+      //App Bar
+      appBar: AppBar(
+          backgroundColor: Colors.blueGrey[700],
+          title: const Center(
+            child: Text(
+              style: TextStyle(color: Colors.white),
+              "Meet Bashboushi",
+            ),
+          )),
+      //Body
+      body: const SafeArea(
+        child: Center(
+          child: Image(
+            //Add in pubspec.yaml
+            // assets:
+            //  - images/
+            image: AssetImage("images/Bashboushi.png"),
+          ),
+        ),
+      ),
+    ),
+  ));
+}
