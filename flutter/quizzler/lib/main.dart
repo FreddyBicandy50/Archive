95e3c40e2b09523b7a0d88c1f9ac6bc4818ff580
FreddyBicandy50 <fbicandy@Freddy-PC>
2024-10-20 16:08:52 +0300

    upload

---
+++ flutter/quizzler/lib/main.dart
@@ -0,0 +1,113 @@
+import 'package:flutter/material.dart';
+import 'questionbank.dart';
+
+void main() => runApp(const Quizzler());
+
+class Quizzler extends StatelessWidget {
+  const Quizzler({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.grey.shade900,
+        body: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10.0),
+            child: QuizPage(),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class QuizPage extends StatefulWidget {
+  @override
+  _QuizPageState createState() => _QuizPageState();
+}
+
+class _QuizPageState extends State<QuizPage> {
+  QuestionBank questions = QuestionBank();
+  String currentText = '';
+  int index = 0;
+  List<Widget> score = [];
+
+  Expanded ftButton({var setColor, var setText}) {
+    return Expanded(
+      child: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Container(
+          color: setColor,
+          child: TextButton(
+              child: Text(
+                setText,
+                style: const TextStyle(fontSize: 20.0, color: Colors.white),
+              ),
+              onPressed: () {
+                setState(() {
+                  if (index < questions.length()) {
+                    index++;
+                    if (setText == "True") {
+                      questions.answers(true);
+                      score.add(
+                        const Expanded(
+                          child: Padding(
+                            padding: EdgeInsets.all(8.0),
+                            child: Icon(Icons.check, color: Colors.green),
+                          ),
+                        ),
+                      );
+                    } else if (setText == "False") {
+                      questions.answers(true);
+                      score.add(
+                        const Expanded(
+                          child: Padding(
+                            padding: EdgeInsets.all(8.0),
+                            child: Icon(Icons.close, color: Colors.red),
+                          ),
+                        ),
+                      );
+                    }
+                    currentText = questions.displayText(index);
+                  }
+                });
+              }),
+        ),
+      ),
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    currentText = questions.displayText(index);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        Expanded(
+          flex: 5,
+          child: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Center(
+              child: Text(
+                currentText,
+                textAlign: TextAlign.center,
+                style: const TextStyle(fontSize: 25.0, color: Colors.white),
+              ),
+            ),
+          ),
+        ),
+        ftButton(setColor: Colors.green, setText: "True"),
+        ftButton(setColor: Colors.red, setText: "False"),
+        Row(children: score)
+      ],
+    );
+  }
+}
---
+++ flutter/quizzler/lib/questionbank.dart
@@ -0,0 +1,63 @@
+import 'question.dart';
+
+class QuestionBank {
+  int totalScore = 0;
+  List<bool> _answers = [];
+  final List<Question> _questions = [
+    Question('Jesus has gave us the word of eternal life to follow him and die',
+        false),
+    Question(
+        'When someone has face "Redemption" he often will lose it soon', false),
+    Question(
+        'A man can\'t runaway or control his lust unless he is constantly practicing salvation by prayers and hymns',
+        true),
+    Question(
+        'The way of a true Redemption is often come\'s with turbulence and trials to make the person weaker and to see if he quits',
+        true),
+    Question('The Redemption of a man is often fated', true),
+    Question('Some cats are actually allergic to humans', true),
+    Question('You can lead a cow down stairs but not up stairs.', false),
+    Question('Approximately one quarter of human bones are in the feet.', true),
+    Question('A slug\'s blood is green.', true),
+    Question('Buzz Aldrin\'s mother\'s maiden name was \"Moon\".', true),
+    Question('It is illegal to pee in the Ocean in Portugal.', true),
+    Question(
+        'No piece of square dry paper can be folded in half more than 7 times.',
+        false),
+    Question(
+        'In London, UK, if you happen to die in the House of Parliament, you are technically entitled to a state funeral, because the building is considered too sacred a place.',
+        true),
+    Question(
+        'The loudest sound produced by any animal is 188 decibels. That animal is the African Elephant.',
+        false),
+    Question(
+        'The total surface area of two human lungs is approximately 70 square metres.',
+        true),
+    Question('Google was originally called \"Backrub\".', true),
+    Question(
+        'Chocolate affects a dog\'s heart and nervous system; a few ounces are enough to kill a small dog.',
+        true),
+    Question(
+        'In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.',
+        true),
+  ];
+
+  String displayText(int index) {
+    if (index == _questions.length) {
+      index++;
+      int i = 0;
+      while (i < _questions.length) {
+        if (_answers[i] == _questions[i].questionAnswer) {
+          totalScore++;
+        }
+        i++;
+      }
+      return "Thank you for helping \n score $totalScore out of ${_questions.length}";
+    }
+    if (index < _questions.length) return _questions[index].questionText;
+    return '';
+  }
+
+  int length() => _questions.length;
+  void answers(bool a) => _answers.add(a);
+}
