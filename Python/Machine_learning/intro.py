c7fd054a78bb3079b0508de30d2d889dec2b74a3
FreddyBicandy50 <fbicandy@Freddy-PC>
2024-10-24 19:17:48 +0300

    upload

--- Python/Machine_learning/intro.py
+++ Python/Machine_learning/intro.py
@@ -1,3 +1,38 @@
-x=int(input("f(x):"))
-F=2*pow(x,2)+3*x+5
-print(f"f=2*x^2+3x+5\n ={F}")
+#2
+mylist=[1,2,3,4,2,3,4,5,6,7,8,9,5,6,1,2,3,4,5,6,7,8]
+myset=set(mylist)
+print(myset)
+
+
+#2
+list=[1,2,4,5,6,7,8,9]
+list2=[3,2,1,9]
+print(set(list) & set(list2))
+
+
+#3
+list=[1,2,4,5,6,7,8,9]
+list2=[3,2,1,9]
+print(set(list) & set(list2))
+
+
+#4
+
+pname=input("enter product name:")
+price=input("enter price:")
+print(f"The Product name:{pname} with price:{price}")
+
+
+#5
+list=[]
+count=0
+
+while(1):
+    word=input("~")
+    if len(word) >=1:
+        list.append(word)
+        count+=1
+    if (word=="END"):
+        break
+    
+print(list[0:len(list) - 1])
